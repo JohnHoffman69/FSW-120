9e61514aa9d62c9b624d9a433de4baa959f21375
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-02-21 22:09:56 -0800

    updateToDo

---
+++ week1/todo/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function App() {
+  return (
+    <div>
+      <p>Check List</p>
+      <input type="checkbox" />Take out the trash<br/>
+      <input type="checkbox" />Work out<br/>
+      <input type="checkbox" />Feed the kids<br/>
+  
+    </div>
+  );
+}
+
+export default App;
