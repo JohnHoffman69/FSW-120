84c75c009c83f8b91c4941aa03185de6ea9ef0cc
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-03-01 22:36:50 -0800

    Blog Props

---
+++ week2/Blog-Props/box/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="Footcontainer">
+        Copyright © John Hoffman 2021
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ week2/Blog-Props/box/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+<nav className="nav">
+  <span className="navbarLogo">Start Bootstrap</span>
+  <span><a className="nav-link" href="#">HOME</a></span>
+  <span><a className="nav-link" href="#">ABOUT</a></span>
+  <span><a className="nav-link" href="#">SAMPLE POST </a></span>
+  <span><a className="nav-link" href="#">CONTACT </a></span>
+  
+</nav>
+    )
+}
+
+export default Navbar;
