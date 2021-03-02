c5d1a791d082de46354ac720474bcf0e7883602c
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-03-01 22:52:04 -0800

    update Colored-Info-Boxes

--- week2/Blog-Props/box/src/BlogProps.js
+++ week2/Blog-Props/box/src/BlogProps.js
@@ -5,7 +5,7 @@ let BlogProps = (props) => {
         <div className="propBoxes">
             <p className="propTitle">{props.title}</p>
             <p className="propsubTitle">{props.subTitle}</p>
-            <p className="authorship">Posted by <span className="authored">{props.author}</span> {props.date}</p>
+            <p className="originator">Posted by <span className="authored">{props.author}</span> {props.date}</p>
             <hr></hr>
         </div>
     )
